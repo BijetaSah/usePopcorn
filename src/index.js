312f17f395776983d56b68b8260988056267dad5
BijetaSah <bijetasah@30gmail.com>
2025-05-07 22:32:30 +0530

    Created a reusable star component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./starRating";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating
+        maxRating={10}
+        color="purple"
+        size={32}
+        onSetRating={setMovieRating}
+      />
+      <p>This movie is {movieRating} rated</p>
+    </div>
+  );
+}
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+
+    <StarRating
+      maxRating={5}
+      color="#fc4"
+      size="24"
+      className=""
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    {/* <StarRating maxRating={5} color="blue" size={48} /> */}
+    <Test />
   </React.StrictMode>
 );
